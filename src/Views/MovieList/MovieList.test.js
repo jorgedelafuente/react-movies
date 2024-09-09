d1e88df551e455bd73b047c97a1cc59043764f07
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2024-09-09 17:25:46 +0200

    git init with vite

--- src/Views/MovieList/MovieList.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../../App";
-
-test("Renders search input on load", () => {
-  render(<App />);
-  const input = screen.queryByPlaceholderText(/Use Enter Key/i);
-  expect(input).toBeInTheDocument();
-});
