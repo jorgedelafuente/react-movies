524981c0d6f91ad2ebb1de12cab6f5ed7d68049f
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2024-09-29 23:43:43 +0200

    styles : implement tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+   theme: {
+      extend: {
+         colors: {
+            'primary-background-color': 'var(--primary-background-color)',
+            'secondary-background-color': 'var(--secondary-background-color)',
+         },
+      },
+   },
+   plugins: [],
 };
