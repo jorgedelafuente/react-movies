fc283c1ef903df5384eaff4033110d6b98e45749
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2024-09-30 00:20:44 +0200

    tests : refactor input test with beforeeach

--- src/components/layout/navbar/search-input/search-input.spec.tsx
+++ src/components/layout/navbar/search-input/search-input.spec.tsx
@@ -1,29 +1,28 @@
 import { afterEach, describe, expect, it } from 'vitest';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup, fireEvent, screen } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import SearchInput from './search-input';
 
-afterEach(cleanup);
-
 describe('Search Input', () => {
-   it('search input should start with empty default state', () => {
-      const { getByTestId } = render(
+   beforeEach(() => {
+      render(
          <QueryClientProvider client={new QueryClient()}>
             <SearchInput />
          </QueryClientProvider>
       );
-      const { textContent } = getByTestId('custom-input');
+   });
+   afterEach(() => {
+      cleanup();
+   });
+
+   it('search input should start with empty default state', () => {
+      const { textContent } = screen.getByTestId('custom-input');
       expect(textContent).toBe('');
    });
 
    it('search input value changes correctly', () => {
-      const { getByTestId } = render(
-         <QueryClientProvider client={new QueryClient()}>
-            <SearchInput />
-         </QueryClientProvider>
-      );
-      const input = getByTestId('custom-input') as HTMLInputElement;
+      const input = screen.getByTestId('custom-input') as HTMLInputElement;
       fireEvent.change(input, { target: { value: 'Search' } });
       expect(input.value).toBe('Search');
    });
