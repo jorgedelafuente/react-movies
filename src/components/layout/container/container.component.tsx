13eba826ecdd61cacab40eb291a7a7bb3a8c706e
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2024-09-29 23:43:43 +0200

    styles : implement tailwind variables

--- src/components/layout/container/container.component.tsx
+++ src/components/layout/container/container.component.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const Container = ({ children }: { children: ReactNode }) => {
    return (
-      <div className="flex flex-row flex-wrap justify-center gap-6 bg-black p-6 sm:p-4 md:p-10 lg:p-10">
+      <div className="bg-primary-background-color flex flex-row flex-wrap justify-center gap-6 p-6 sm:p-4 md:p-10 lg:p-10">
          {children}
       </div>
    );
