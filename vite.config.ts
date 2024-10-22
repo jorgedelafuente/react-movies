5da73bdc7e57b28a7f6e4747da9a90f97a2fb8de
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2024-10-22 17:30:51 +0200

    test : fix playwright config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { configDefaults } from 'vitest/config';
 import { resolve } from 'path';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
@@ -20,6 +21,7 @@ export default defineConfig({
       coverage: {
          provider: 'istanbul',
       },
+      exclude: [...configDefaults.exclude, 'src/tests/\\e2e\\/*'],
    },
    resolve: {
       alias: {
