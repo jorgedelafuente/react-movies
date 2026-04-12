c2600bed20302e3e0c0085f70c64b43cf2559c9c
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2026-04-12 18:14:55 +0200

    refactor: remove unused test

--- src/components/layout/navbar/navbar.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Navbar from './navbar.component';
-
-describe('Navbar', () => {
-   it('the navbar title is in the document', () => {
-      render(<Navbar title="custom_title" />);
-      expect(screen.getByText('custom_title')).toBeInTheDocument();
-   });
-});
