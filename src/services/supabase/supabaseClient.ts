0af90614c1c9428925cc25dfda65f210f3784839
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2026-04-13 21:41:42 +0200

    chore: fix tests

--- src/services/supabase/supabaseClient.ts
+++ src/services/supabase/supabaseClient.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl =
+   import.meta.env.VITE_SUPABASE_URL ?? process.env.VITE_SUPABASE_URL ?? '';
+const supabaseAnonKey =
+   import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY ??
+   process.env.VITE_SUPABASE_PUBLISHABLE_KEY ??
+   '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
    auth: {
