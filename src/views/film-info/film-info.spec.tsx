bc23b58ec564548d81ce11ef96fa9d37bcbd94d3
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2026-04-21 08:22:08 +0200

    test: fix test with provider

--- src/views/film-info/film-info.spec.tsx
+++ src/views/film-info/film-info.spec.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
 
 import { MOCK_FILM_INFO, MOCK_FILM_TRAILER } from '@/tests/__mocks__/mocks';
@@ -8,10 +9,14 @@ const MOCK_TITLE = 'Deadpool &amp; Wolverine';
 
 describe('Film Info Component', () => {
    it('Film Info title is visible', () => {
+      const queryClient = new QueryClient();
       const { getByTestId } = render(
-         <FilmInfo filmInfo={MOCK_FILM_INFO} filmTrailer={MOCK_FILM_TRAILER} />
+         <QueryClientProvider client={queryClient}>
+            <FilmInfo filmInfo={MOCK_FILM_INFO} filmTrailer={MOCK_FILM_TRAILER} />
+         </QueryClientProvider>
       );
       const headingElement = getByTestId('film-info-title');
       expect(headingElement.innerHTML).toEqual(MOCK_TITLE);
+      queryClient.clear();
    });
 });
